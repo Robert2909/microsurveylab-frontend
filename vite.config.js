315dba83720ebd24b3cd23de6b95e0ceed4bfe69
Robert2909 <alfredman2909@gmail.com>
2025-12-09 23:57:01 -0600

    Detalles finales

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// Cambia TU_USUARIO por tu usuario real de GitHub
 export default defineConfig({
   plugins: [svelte()],
   base: '/microsurveylab-frontend/'
