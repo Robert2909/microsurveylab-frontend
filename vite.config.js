ae097c0b86fc8adc9d7f6d6f31897513310a50dc
Robert2909 <alfredman2909@gmail.com>
2025-12-09 18:47:27 -0600

    Agregar workflow de deploy a Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
+// Cambia TU_USUARIO por tu usuario real de GitHub
 export default defineConfig({
   plugins: [svelte()],
+  base: '/microsurveylab-frontend/'
 })
